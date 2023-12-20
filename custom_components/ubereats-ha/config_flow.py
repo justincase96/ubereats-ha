8977fed2a8b7257be19befa439f7634fefe7b0f8
justincase96 <62328647+justincase96@users.noreply.github.com>
2023-12-20 13:17:18 +0200

    Update config_flow.py

---
+++ custom_components/ubereats-ha/config_flow.py
@@ -0,0 +1,25 @@
+from homeassistant import config_entries
+
+class UberEatsConfigFlow(config_entries.ConfigFlow, domain="uber_eats"):
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_CLOUD_POLL
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="Uber Eats", data=user_input)
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required("cookie"): str,
+                    vol.Required("userUuid"): str,
+                    vol.Required("timezone", default="Africa/Johannesburg"): vol.In([
+                        "Africa/Johannesburg",
+                        "America/New_York",
+                        "Asia/Tokyo",
+                        # Add more timezones here
+                    ]),
+                }
+            ),
+        )
